5a9cfb4448657d27139edf951594bda7ee4348e7
Durgesh <durgesh.kumar@panopticinfotech.com>
2026-06-17 00:59:21 +0530

    Refactor CORS middleware to use default settings for improved security

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import cors from "cors"
 const app = express();
 app.set("trust proxy", 1);
 app.use(express.json({ limit: "5mb" }));
-app.use(cors("*"));
+app.use(cors());
 // app.use(authenticate);
 app.use(apiLimiter);
 
