5d7398cab6d88d375e866d6d8d68b3edf9f3dba8
Durgesh <durgesh.kumar@panopticinfotech.com>
2026-06-17 00:55:59 +0530

    Add CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import express from "express";
 import aiRoutes from "./routes/ai.routes.js";
 import { authenticate } from "./middlewares/auth.middleware.js";
 import { apiLimiter } from "./middlewares/rateLimit.middleware.js";
+import cors from "cors"
 
 const app = express();
 app.set("trust proxy", 1);
 app.use(express.json({ limit: "5mb" }));
-
+app.use(cors("*"));
 // app.use(authenticate);
 app.use(apiLimiter);
 
